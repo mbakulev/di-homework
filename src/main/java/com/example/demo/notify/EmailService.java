625623ba001824a07343e8a77cb384c16b983bd0
Mikhail Bakulev <michael.baculev@gmail.com>
2025-05-11 12:30:19 +0400

    init commit

---
+++ src/main/java/com/example/demo/notify/EmailService.java
@@ -0,0 +1,14 @@
+package com.example.demo.notify;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(name = "notify.mode", havingValue = "email")
+public class EmailService implements NotificationService {
+    @Override
+    public void send(String message) {
+        System.out.println("Отправка Email: " + message);
+    }
+}
---
+++ src/main/java/com/example/demo/notify/SmsService.java
@@ -0,0 +1,15 @@
+package com.example.demo.notify;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(name = "notify.mode", havingValue = "sms")
+public class SmsService implements NotificationService {
+    @Override
+    public void send(String message) {
+        System.out.println("Отправка SMS: " + message);
+    }
+}
